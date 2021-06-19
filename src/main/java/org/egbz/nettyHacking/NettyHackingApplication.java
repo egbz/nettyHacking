1ffa32757c5c7db4bb56d69b26c374a3af841538
egbz <1023271893@qq.com>
2021-06-19 15:34:05 +0800

    amend

---
+++ src/main/java/org/egbz/nettyHacking/NettyHackingApplication.java
@@ -0,0 +1,14 @@
+package org.egbz.nettyHacking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyHackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyHackingApplication.class, args);
+		System.out.println("hi there");
+	}
+
+}
---
+++ src/test/java/org/egbz/nettyHacking/NettyHackingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.egbz.nettyHacking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyHackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
