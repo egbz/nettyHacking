77533518e83e6f1d19550cca46811c3681116b17
egbz <1023271893@qq.com>
2021-07-02 18:45:06 +0800

    amend

--- src/main/java/org/egbz/nettyHacking/discardServer/DiscardServerhandler.java
+++ src/main/java/org/egbz/nettyHacking/discardServer/DiscardServerhandler.java
@@ -12,21 +12,27 @@ import io.netty.util.ReferenceCountUtil;
  */
 public class DiscardServerhandler extends ChannelInboundHandlerAdapter {
 
+    // [discard server]
+    // print and discard
+//    @Override
+//    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+//        ByteBuf in = (ByteBuf) msg;
+//        try {
+//            while (in.isReadable()) {
+//                // 打印收到的消息
+//                System.out.print((char) in.readByte());
+//                System.out.flush();
+//            }
+//        } finally {
+//            ReferenceCountUtil.release(msg);
+//        }
+//    }
+
+    // [echo server]
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
-        // Discard the received data silently
-//        ((ByteBuf) msg).release();
-
-        ByteBuf in = (ByteBuf) msg;
-        try {
-            while (in.isReadable()) {
-                // 打印收到的消息
-                System.out.print((char) in.readByte());
-                System.out.flush();
-            }
-        } finally {
-            ReferenceCountUtil.release(msg);
-        }
+        ctx.write(msg);
+        ctx.flush();
     }
 
     @Override
