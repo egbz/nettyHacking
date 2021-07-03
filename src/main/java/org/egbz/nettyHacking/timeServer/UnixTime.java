84c8ecbc8bbc3704c0286bee6934a796fa3641ad
egbz <1023271893@qq.com>
2021-07-03 12:30:32 +0800

    amend

---
+++ src/main/java/org/egbz/nettyHacking/timeServer/UnixTime.java
@@ -0,0 +1,29 @@
+package org.egbz.nettyHacking.timeServer;
+
+import java.util.Date;
+
+/**
+ * @author egbz
+ * @date 2021/7/3
+ */
+public class UnixTime {
+
+    private final long value;
+
+    public UnixTime(long value) {
+        this.value = value;
+    }
+
+    public UnixTime() {
+        this(System.currentTimeMillis() / 1000L + 2208988800L);
+    }
+
+    public long value() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return new Date((value() - 2208988800L) * 1000L).toString();
+    }
+}
