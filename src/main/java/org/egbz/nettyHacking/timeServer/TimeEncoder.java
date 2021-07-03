9e90e608b034a29c80630e6edbb9f714c801d4d7
egbz <1023271893@qq.com>
2021-07-03 12:40:47 +0800

    amend

---
+++ src/main/java/org/egbz/nettyHacking/timeServer/TimeEncoder.java
@@ -0,0 +1,22 @@
+package org.egbz.nettyHacking.timeServer;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelOutboundHandler;
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
+
+/**
+ * @author egbz
+ * @date 2021/7/3
+ */
+public class TimeEncoder extends ChannelOutboundHandlerAdapter {
+
+    @Override
+    public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) {
+        UnixTime m = (UnixTime) msg;
+        ByteBuf encoded = ctx.alloc().buffer(4);
+        encoded.writeInt((int) m.value());
+        ctx.write(encoded, promise);
+    }
+}
--- src/main/java/org/egbz/nettyHacking/timeServer/TimeServerHandler.java
+++ src/main/java/org/egbz/nettyHacking/timeServer/TimeServerHandler.java
@@ -15,17 +15,8 @@ public class TimeServerHandler extends ChannelInboundHandlerAdapter {
 
     @Override
     public void channelActive(final ChannelHandlerContext ctx) {
-        final ByteBuf time = ctx.alloc().buffer(4);
-        time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
-
-        final ChannelFuture f = ctx.writeAndFlush(time);
-        f.addListener(new ChannelFutureListener() {
-            @Override
-            public void operationComplete(ChannelFuture future) throws Exception {
-                assert f == future;
-                ctx.close();
-            }
-        });
+     ChannelFuture f = ctx.writeAndFlush(new UnixTime());
+     f.addListener(ChannelFutureListener.CLOSE);
     }
 
     @Override
