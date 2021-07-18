b741190b10da6ca22d4896111fb9fb75135fcf3c
egbz <1023271893@qq.com>
2021-07-18 16:58:04 +0800

    amend

---
+++ src/main/java/org/egbz/nettyHacking/test/AbsIntegerEncoder.java
@@ -0,0 +1,22 @@
+package org.egbz.nettyHacking.test;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageEncoder;
+
+import java.util.List;
+
+/**
+ * @author egbz
+ * @date 2021/7/18
+ */
+public class AbsIntegerEncoder extends MessageToMessageEncoder<ByteBuf> {
+    @Override
+    protected void encode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
+        while (msg.readableBytes() >= 4) {
+            int value = Math.abs(msg.readInt());
+            out.add(value);
+        }
+
+    }
+}
