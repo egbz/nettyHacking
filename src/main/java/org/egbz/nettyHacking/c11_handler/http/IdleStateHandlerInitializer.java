7744e60f31a26f9fb99ada60b63b985f01367d4e
egbz <1023271893@qq.com>
2021-07-22 22:12:53 +0800

    amend

---
+++ src/main/java/org/egbz/nettyHacking/c11_handler/http/IdleStateHandlerInitializer.java
@@ -0,0 +1,40 @@
+package org.egbz.nettyHacking.c11_handler.http;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
+import io.netty.util.CharsetUtil;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author egbz
+ * @date 2021/7/22
+ */
+public class IdleStateHandlerInitializer extends ChannelInitializer<Channel> {
+
+    @Override
+    protected void initChannel(Channel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast(
+                new IdleStateHandler(0, 0, 60, TimeUnit.SECONDS));
+        pipeline.addLast(new HeartbeatHandler());
+    }
+
+    public static final class HeartbeatHandler extends ChannelInboundHandlerAdapter {
+        private static final ByteBuf HEARTBEAT_SEQUENCE =
+                Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("HEARTBEAT", CharsetUtil.ISO_8859_1));
+
+        @Override
+        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+            if (evt instanceof IdleStateEvent) {
+                ctx.writeAndFlush(HEARTBEAT_SEQUENCE.duplicate())
+                        .addListener(ChannelFutureListener.CLOSE_ON_FAILURE);
+            } else {
+                super.userEventTriggered(ctx, evt);
+            }
+        }
+    }
+}
